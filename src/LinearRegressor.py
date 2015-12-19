5b623266db378ee9efd04cabd635d12234776dbb
Julia Beltrao <julia.ramos.beltrao@gmail.com>
2015-12-19 23:35:10 +0200

    Linear Regressor fixed.

--- src/LinearRegressor.py
+++ src/LinearRegressor.py
@@ -1,38 +1,72 @@
-#!/usr/bin/python
-
 import numpy as np
+import sys
 
 class LinearRegressor:
 
 	def fit(self, x, r):
-		alfa = 0.1
+		alfa = 0.0001
+		epsilon = 0.00001
+		#lfa = 0.2
 		Ec = float('inf')
-		x0 = np.ones(5000) 
-		W = np.ones((10, 1))
-		
+		dim = np.shape(x)
+		x0 = np.ones((dim[0],1)) 
+		#W = np.zeros((dim[1]+1, 1))
+		W = np.array([[-53], [-0.08], [1], [-0.003], [-0.04], [0.8], [-0.005], [0.002], [62], [-0.55], [-0.6], [-0.25]])
+		#print W
+		#sys.exit(1)
+
 		x = np.insert(x, [0], x0, axis=1)	
+		#print "x", np.shape(x)
+		#print x[0:10:1, 0:2:1]
 	
 		while 1: 
-			h = np.dot(x, W)		
-			e = np.subtract(h, r)
+			h = np.dot(x, W)
+			#print h[0:50:1,0]
+			#print x[0:10:1,0:12:1]
+			#for i in range(3000):
+			#	if h[i,0] != np.dot(W.reshape(12), x[i,:]):
+			#		print "deu ruim!", h[i,0], "!=", np.dot(W.reshape(12), x[i,:])
+			#sys.exit(1)
+			#print "x", np.sum(x[0, :])
+			#print "w", W
+			#print "h", h[0,0]
+			e = np.subtract(h, np.array(r, dtype=np.float))
+			#print h[0:10:1,0] 
+			#print r[0:10:1,0] 
+			#print e[0:10:1,0]
+			#sys.exit(1)
+			#print "r", np.shape(r)
+			#print "h", np.shape(h)
+			#print "e", np.shape(e)
 
 			Ep = Ec
-			Ec = np.sum(np.dot(e, e))
-
-			if Ec > Ep:
+			Ec = np.sum(np.square(e)) / float(2*dim[0])
+			print "Ec", Ec
+			#sys.exit(1)
+			if Ep - Ec < epsilon:
 				break
 
 			f = np.dot(e.T, x)
-			W = np.subtract(W, multiply(alfa, f))
+			f = np.divide(dim[0], f)
+			#print "eT", np.shape(e)
+			#print "x", np.shape(x[:,0])
+			#print "f", f[0,0]
+			#print "eTx", np.sum(np.multiply(e.reshape(3000),x[:,0].reshape(3000)))
+			#print "f", np.shape(f)
+			#print "w", W
+			W = np.subtract(W, np.multiply(alfa, f.T))
+			#print W
+			#print f.T
+			#sys.exit(1)
 
 		self.Wopt = W
 		
 	def predict(self, x):
-
-		x0 = np.ones(5000) 
+		dim = np.shape(x)
+		x0 = np.ones((dim[0],1)) 
 		x = np.insert(x, [0], x0, axis=1)	
 		
 		h = np.dot(x, self.Wopt)
 
 		self.g = np.around(h)		
-		
+
--- src/Test_LinearRegressor.py
+++ src/Test_LinearRegressor.py
@@ -2,31 +2,40 @@ import numpy as np
 import csv
 from LinearRegressor import LinearRegressor
 
-reader = csv.reader(open("training.csv"), delimiter=',')
+reader = csv.reader(open("../data/training_classification_regression_2015.csv"), delimiter=',')
 data = np.asarray(list(reader))
 data = np.delete(data, 0, axis=0)
 
-x = data[:, 0:11:1]
-r = data[:, 11]
+x = np.array(data[:, 0:11:1], dtype=float)
+r = np.array(data[:, 11], dtype=float)
+r = r.reshape((np.shape(r)[0],1))
 #x = np.delete(data, 12, axis=1)
 #x = np.delete(x, 11, axis=1)
 
 regressor = LinearRegressor()
 
-x_train = x[0:3000:1, :]
-r_train = r[0:3000:1]
+x_train = x[0:4000:1, :]
+r_train = r[0:4000:1]
 regressor.fit(x_train, r_train)
 
 ##############################
 
 
-x_test = x[3000:5001:1, :]
-r_test = r[3000:5001:1]
+x_test = x[4000:5001:1, :]
+r_test = r[4000:5001:1]
 
 regressor.predict(x_test)
 
+m = regressor.g
+#print np.shape(m)
+print m[0:50:1, 0]
+
 e = np.subtract(regressor.g, r_test)
-Et = np.sum(np.dot(e, e))
+#Et = np.sum(np.dot(e, e))
+s = np.shape(e)
+Et = np.sum(np.dot(e.reshape(s[0]), e.reshape(s[0]))) / float(2*1000)
 
-print Et
+accurracy = np.sum(np.array(m, dtype=np.int) == r_test) / float(np.shape(r_test)[0])
 
+print "Et", Et
+print accurracy
